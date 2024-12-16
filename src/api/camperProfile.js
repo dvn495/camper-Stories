a94179e4ce04b194f25308fba909b8cb839faafb
santilaguado <santiagolaguado57@gmail.com>
2024-12-16 13:37:50 -0500

    Feat: Trajabando en el Carrusel Principal

--- src/api/camperProfile.js
+++ src/api/camperProfile.js
@@ -8,26 +8,26 @@ const profiles = [
     name: "Natalia Diaz Suarez",
     title: "Fullstack Software Developer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natusque justo suscipit voluptatem accusamus.",
+      "“Lorem ipsum dolor sit amet consectetur adipiscing elit natoque justo suscipit phasellus senectus duis, morbi himenaeos ligula dapibus fermentum pellentesque vivamus vulputate mollis magnis sed.”",
     image: camperImage, // Usar la imagen importada
     buttonText: "Más Información",
   },
   {
     id: 2,
-    name: "John Doe",
+    name: "Santiago Laguado Osorio",
     title: "Frontend Developer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natusque justo suscipit voluptatem accusamus.",
-    image: "https://via.placeholder.com/150", // Cambia esto por una URL real
+      "“Lorem ipsum dolor sit amet consectetur adipiscing elit natoque justo suscipit phasellus senectus duis, morbi himenaeos ligula dapibus fermentum pellentesque vivamus vulputate mollis magnis sed.”",
+    image: camperImage,
     buttonText: "Más Información",
   },
   {
     id: 3,
-    name: "Jane Smith",
+    name: "Cristopher Daniel Buitrago",
     title: "Backend Engineer",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natusque justo suscipit voluptatem accusamus.",
-    image: "https://via.placeholder.com/150", // Cambia esto por una URL real
+      "“Lorem ipsum dolor sit amet consectetur adipiscing elit natoque justo suscipit phasellus senectus duis, morbi himenaeos ligula dapibus fermentum pellentesque vivamus vulputate mollis magnis sed.”",
+    image: camperImage,
     buttonText: "Más Información",
   },
 ];
