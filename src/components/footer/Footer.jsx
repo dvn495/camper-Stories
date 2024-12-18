2ef0034158a5c51aa77128b19134110dc11d15a9
santilaguado <santiagolaguado57@gmail.com>
2024-12-18 17:13:37 -0500

    Feat: Se trabaja en el Responsive de las secciones: CampersGrid, FormSection, MainCampers, Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import "./styles/Footer.css";
+  import React from 'react';
+  import "./styles/Footer.css";
 
-const Footer = () => {
-  return (
-    <footer className="footer">
+  const Footer = () => {
+    return (
+      <footer className="footer">
 
-      <div className='footer-container'>
-        <img src="src/assets/CampusLogo.png" alt="Campuslands" />
-        <div className='line'></div>
-        <div className='underline-content'>
-          <p>© Campus 2024 - Todos los derechos Reservados</p>
-          <div className='social-logos'>
-            <a href="https://www.facebook.com/Campuslands/" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-facebook" aria-hidden="true"></i>
-            </a>
-            <a href="https://www.instagram.com/campuslands/" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-            <a href="https://www.linkedin.com/company/campuslands/" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a href="https://www.youtube.com/@campuslands" target="_blank" rel="noopener noreferrer">
-              <i className="fa fa-youtube-play" aria-hidden="true"></i>
-            </a>
+        <div className='footer-container'>
+          <img src="src/assets/CampusLogo.png" alt="Campuslands" />
+          <div className='line'></div>
+          <div className='underline-content'>
+            <p>© Campus 2024 - Todos los derechos Reservados</p>
+            <div className='social-logos'>
+              <a href="https://www.facebook.com/Campuslands/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-facebook" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.instagram.com/campuslands/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.linkedin.com/company/campuslands/" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-linkedin" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.youtube.com/@campuslands" target="_blank" rel="noopener noreferrer">
+                <i className="fa fa-youtube-play" aria-hidden="true"></i>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
-  );
-};
+      </footer>
+    );
+  };
 
-export default Footer;
+  export default Footer;
