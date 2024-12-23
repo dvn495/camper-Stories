15d21b0129376d656bbbdfa3adc13ae272706b13
mizamarzes <mizamarzes@gmail.com>
2024-12-23 15:06:22 -0500

    CamperCRUD - pruebas

--- backend/helpers/conexion.js
+++ backend/helpers/conexion.js
@@ -4,8 +4,8 @@ class Conexion {
     constructor() {
         this.config = {
             host: process.env.DB_HOST || 'localhost',
-            user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || '12345678',
+            user: process.env.DB_USER || 'campus2023',
+            password: process.env.DB_PASSWORD || 'campus2023',
             port: process.env.DB_PORT || 3306,
             database: process.env.DB_NAME || 'campuslands',
         };
