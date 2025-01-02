6d7c9956f1f86cc52979047c938bdb393219aec9
mizamarzes <mizamarzes@gmail.com>
2025-01-02 13:44:05 -0500

    cambios en el dreams grid

---
+++ src/data/data.js
@@ -0,0 +1,27 @@
+// dreamsData.js
+import paisaje from '../assets/paisaje-prueba.jpeg';
+
+export const dreamsData = {
+ dreams: [
+   {
+     id: 1,
+     title: "Graduación Universitaria",
+     description: "Obtener mi título en Ingeniería de Software",
+     image: paisaje 
+   },
+   {
+     id: 2, 
+     title: "Empresa Propia",
+     description: "Fundar mi startup de tecnología",
+     image: paisaje
+   },
+   {
+    id: 3, 
+    title: "Empresa Propia",
+    description: "Fundar mi startup de tecnología",
+    image: paisaje
+   }
+ ]
+};
+
+export default dreamsData;
